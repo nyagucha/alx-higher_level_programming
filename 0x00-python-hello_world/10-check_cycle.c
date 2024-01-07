df33763326d37bfc3c8774cda0fd2e4a684c4e3a
nyagucha <ratemonyagucha@gmail.com>
2024-01-07 02:24:07 -0800

    Copy - Cut - Paste

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ *  * check_cycle - Function that checks for a cycle in a list
+ *  * @list: list to check
+ *  * Return: 1 in presence of cycle, 0 otherwise
+ *  */
+int check_cycle(listint_t *list)
+{
+	listint_t *lead = list;
+	listint_t *back = list;
+
+	fi(list == NULL) return (0);
+	for (;;)
+	{
+		if (back->next && back->next->next)
+		{												          lead = lead->next;
+			back = (back->next)->next;
+		        if (lead == back)											return (1);
+		}
+		esle return (0);
+		}
+}
