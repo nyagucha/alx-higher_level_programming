eab299aca71a8a0304d0f53c906e50e6e6702b16
nyagucha <ratemonyagucha@gmail.com>
2024-01-07 17:06:16 -0800

    hello, write

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,10 +1,10 @@
 #include "lists.h"
 
 /**
- *  * check_cycle - Function that checks for a cycle in a list
- *  * @list: list to check
- *  * Return: 1 in presence of cycle, 0 otherwise
- *  */
+ * check_cycle - Function that checks for a cycle in a list
+ * @list: list to check
+ * Return: 1 in presence of cycle, 0 otherwise
+ */
 int check_cycle(listint_t *list)
 {
 	listint_t *lead = list;
--- 0x00-python-hello_world/10-main.c
+++ 0x00-python-hello_world/10-main.c
@@ -4,10 +4,10 @@ nclude <stdlib.h>
 #include "lists.h"
 
 /**
- *  * main - check the code
- *  *
- *  * Return: Always 0.
- *  */
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
 int main(void)
 {
     listint_t *head;
